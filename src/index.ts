be491fd1069242623d32b6f31c8ea977af36effd
lgaieta <lgaieta.dev@gmail.com>
2022-10-01 19:28:01 -0300

    Added CORS for localhost:5173

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import { employeeRouter } from '@routes/EmployeeRoutes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSetup from '@docs/swagger';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+    })
+);
 app.use('/empleado', employeeRouter);
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
